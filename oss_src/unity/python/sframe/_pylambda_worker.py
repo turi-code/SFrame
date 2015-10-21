06d2d098568af8a5b2a0c34cf56256a2665aea55
Hoyt Koepke <hoytak@graphlab.com>
2015-10-21 11:44:55 -0700

    Better diagnostics for installation directory.

--- oss_src/unity/python/sframe/_pylambda_worker.py
+++ oss_src/unity/python/sframe/_pylambda_worker.py
@@ -1,7 +1,9 @@
 import sys
+import os
 from ctypes import PyDLL, c_char_p, c_int
 from os.path import split, abspath, join
 from glob import glob
+from itertools import chain
 
 if __name__ == "__main__":
 
@@ -26,16 +28,46 @@ if __name__ == "__main__":
     pylambda_workers = glob(join(main_dir, "libpylambda_worker.*"))
 
     if debug_mode:
+
+        error = False
+        
         print ("Found %d candidade pylambda_worker file(s): \n   %s."
                % (len(pylambda_workers), "\n   ".join(pylambda_workers)))
 
         if len(pylambda_workers) > 1:
             print "WARNING: multiple pylambda worker libraries."
+            error = True
 
         if len(pylambda_workers) == 0:
             print "ERROR: Cannot find pylambda_worker extension library."
+            error = True
+
+        print "Installation Directory Structure: "
+
+        visited_files = []
+
+        def on_error(err):
+            visited_files.append( ("  ERROR", str(err)) )
+
+        for path, dirs, files in os.walk(main_dir, onerror = on_error):
+            for fn in chain(files, dirs):
+                name = join(path, fn)
+                try:
+                    visited_files.append( (name, repr(os.lstat(name))) )
+                except:
+                    visited_files.append( (name, "ERROR calling os.lstat.") )
+
+        def strip_name(n):
+            if n[:len(main_dir)] == main_dir:
+                return "<root>/" + n[len(main_dir):]
+            else:
+                return n
+                
+        print "\n".join( ("  %s: %s" % (strip_name(name), stats))
+                         for name, stats in sorted(visited_files))
 
         print "INFO: Loading pylambda worker library: ", pylambda_workers[0]
+        
     
     pylambda_lib = PyDLL(pylambda_workers[0])
 
