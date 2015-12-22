9bbc4453b2f394799f647895d3a6387d5424a71f
Yucheng Low <ylow@graphlab.com>
2015-12-22 12:26:52 -0800

    Added cpp tests to make egg

--- oss_local_scripts/make_egg.sh
+++ oss_local_scripts/make_egg.sh
@@ -107,12 +107,17 @@ build_source() {
   cd ${WORKSPACE}/${build_type}/oss_src/unity
   # Make
   make -j${NUM_PROCS}
+
+  cd ${WORKSPACE}/${build_type}/oss_test
+  make -j${NUM_PROCS}
   echo -e "\n\n================= Done Build Source ================\n\n"
 }
 
 # Run all unit test
 unit_test() {
   echo -e "\n\n\n================= Running Unit Test ================\n\n\n"
+  cd ${WORKSPACE}/${BUILD_TYPE}
+  ${WORKSPACE}/oss_local_scripts/run_cpp_tests.py -j 1
 
   cd ${WORKSPACE}
   python -c 'import sframe; sframe.sys_util.test_pylambda_worker()'
