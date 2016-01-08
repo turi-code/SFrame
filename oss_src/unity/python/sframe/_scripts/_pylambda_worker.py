c411191a8aed859b78288c3e511caeea97f65a3e
Hoyt Koepke <hoytak@graphlab.com>
2016-01-07 16:30:06 -0800

    Moved library loading to an isolated file to support spark.

--- oss_src/unity/python/sframe/_scripts/_pylambda_worker.py
+++ oss_src/unity/python/sframe/_scripts/_pylambda_worker.py
@@ -1,3 +1,4 @@
+import gl_lib_load_routines as gllib
 import sys
 import os
 import ctypes
@@ -6,40 +7,11 @@ from os.path import split, abspath, join
 from glob import glob
 from itertools import chain
 
-def set_windows_dll_path():
-    """
-    Sets the dll load path so that things are resolved correctly.
-    """
-
-    # Back up to the directory, then to the base directory as this is
-    # in ./_scripts.
-    lib_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-    def errcheck_bool(result, func, args):
-        if not result:
-            last_error = ctypes.get_last_error()
-            if last_error != 0:
-                raise ctypes.WinError(last_error)
-            else:
-                raise OSError
-        return args
-
-    import ctypes.wintypes as wintypes
-
-    # Also need to set the dll loading directory to the main
-    # folder so windows attempts to load all DLLs from this
-    # directory.
-    try:
-        kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
-        kernel32.SetDllDirectoryW.errcheck = errcheck_bool
-        kernel32.SetDllDirectoryW.argtypes = (wintypes.LPCWSTR,)
-        kernel32.SetDllDirectoryW(lib_path)
-    except Exception, e:
-        sys.stderr.write("Error setting DLL load orders: %s (things may still work).\n" % str(e))
-        sys.stderr.flush()
-
 if __name__ == "__main__":
 
+    ############################################################
+    # Set up the logging
+    
     if len(sys.argv) == 1:
         dry_run = True
     else:
@@ -81,78 +53,39 @@ if __name__ == "__main__":
             _write_log("Error opening '%s' for write: %s" % (_write_out_file_name, repr(e)))
             _write_out_file = None
 
+    for s in sys.argv:
+        _write_log("Lambda worker args: \n  %s" % ("\n  ".join(sys.argv)))
+            
     if dry_run:
         print "PyLambda script called with no IPC information; entering diagnostic mode."
 
-    ########################################
-    # Set up the system path. 
+    ############################################################
+    # Load the correct pylambda worker library.
     
-    system_path = os.environ.get("__GL_SYS_PATH__", "")
+    pylambda_lib = gllib.load_internal_ctypes_library(
+        "libpylambda_worker*",
+        info_log_function = _write_log,
+        error_log_function = lambda s: _write_log(s, error=True))
 
-    del sys.path[:]
-    sys.path.extend(p.strip() for p in system_path.split(os.pathsep) if p.strip())
+    ############################################################
+    # Load in the cython lambda workers.  On import, this will resolve
+    # the proper symbols.
 
-    for i, p in enumerate(sys.path):
-        _write_log("  sys.path[%d] = %s. " % (i, sys.path[i]))
-
-    ########################################
-    # Now, import thnigs
-
-    script_path = abspath(sys.modules[__name__].__file__)
-    main_dir = split(split(script_path)[0])[0]
-
-    _write_log("Script directory: %s." % script_path)
-    _write_log("Main program directory: %s." % main_dir)
-
-    for s in sys.argv:
-        _write_log("Lambda worker args: \n  %s" % ("\n  ".join(sys.argv)))
-
-    ########################################
-    # Set the dll load path if we are on windows
-    if sys.platform == 'win32':
-        set_windows_dll_path()
-
-    ########################################
-    # Find the correct main worker library. 
-    pylamda_worker_search_string = join(main_dir, "libpylambda_worker.*")
-    _write_log("Lambda worker search pattern: %s\n" % pylamda_worker_search_string)
-    pylambda_workers = glob(join(main_dir, "libpylambda_worker.*"))
-
-    _write_log("Found %d candidade pylambda_worker file(s): \n   %s."
-               % (len(pylambda_workers), "\n   ".join(pylambda_workers)))
-
-    if len(pylambda_workers) > 1:
-        _write_log("WARNING: multiple pylambda worker libraries.")
-
-    if len(pylambda_workers) == 0:
-        _write_log("ERROR: Cannot find pylambda_worker extension library.", error = True)
-        sys.exit(202)
-
-    _write_log("INFO: Loading pylambda worker library: %s." % pylambda_workers[0])
-
+    main_dir = gllib.get_main_dir()
     module_name = os.path.split(main_dir)[1]
 
     if module_name != "graphlab" and module_name != "sframe":
         _write_log("Module path not sframe or graphlab.", error = True)
         sys.exit(200)
-
-    ########################################
-    # Get the pointers to the cython callback functions that can
-    # actually execute the lambda functions.
-
-    try:
-        pylambda_lib = PyDLL(pylambda_workers[0], mode=ctypes.RTLD_GLOBAL)
-    except Exception, e:
-        _write_log("Error loading lambda library %s: %s" % (pylambda_workers[0], repr(e)), error = True)
-        sys.exit(203)
-
-    # Load in the cython lambda workers.  On import, this will resolve
-    # the proper symbols.
+    
     if module_name == "graphlab":
         import graphlab.cython.cy_pylambda_workers
     else:
         import sframe.cython.cy_pylambda_workers
 
+    ############################################################
+    # Call the proper pylambda worker main function.
+    
     try:
         pylambda_lib.pylambda_worker_main.argtypes = [c_char_p, c_char_p]
         pylambda_lib.pylambda_worker_main.restype = c_int
