7a7430be645ddf1b4b10acbab2452d591edd9b9d
tobyroseman <toby@dato.com>
2016-06-13 13:02:13 -0700

    Standardize python language version string in egg/wheel file name

--- oss_local_scripts/make_egg.sh
+++ oss_local_scripts/make_egg.sh
@@ -250,14 +250,7 @@ package_egg() {
   elif [[ $OSTYPE == msys ]]; then
     EGG_PATH=`ls ${WORKSPACE}/${build_type}/oss_src/unity/python/dist/SFrame-${VERSION_NUMBER}-*-none-*.whl`
 
-    NEW_EGG_PATH=${EGG_PATH}
-    if [[ $PY_MAJOR_VERSION == 3.4 ]]; then
-        NEW_EGG_PATH=${EGG_PATH/-py3-/-cp34-}
-    elif [[ $PY_MAJOR_VERSION == 3.5 ]]; then
-        NEW_EGG_PATH=${EGG_PATH/-py3-/-cp35-}
-    fi
-
-    NEW_EGG_PATH=${NEW_EGG_PATH/-any./-win_amd64.}
+    NEW_EGG_PATH=${EGG_PATH/-any./-win_amd64.}
     if [[ ! ${EGG_PATH} == ${NEW_EGG_PATH} ]]; then
         mv ${EGG_PATH} ${NEW_EGG_PATH}
         EGG_PATH=${NEW_EGG_PATH}
@@ -268,6 +261,20 @@ package_egg() {
     mv ${EGG_PATH} ${NEW_EGG_PATH}
     EGG_PATH=${NEW_EGG_PATH}
   fi
+
+  # Set Python Language Version Number
+    NEW_EGG_PATH=${EGG_PATH}
+    if [[ $PY_MAJOR_VERSION == 3.4 ]]; then
+        NEW_EGG_PATH=${EGG_PATH/-py3-/-cp34-}
+    elif [[ $PY_MAJOR_VERSION == 3.5 ]]; then
+        NEW_EGG_PATH=${EGG_PATH/-py3-/-cp35-}
+    elif [[ $PY_MAJOR_VERSION == 2.7 ]]; then
+        NEW_EGG_PATH=${EGG_PATH/-py2-/-cp27-}
+    fi
+    if [[ ! ${EGG_PATH} == ${NEW_EGG_PATH} ]]; then
+        mv ${EGG_PATH} ${NEW_EGG_PATH}
+        EGG_PATH=${NEW_EGG_PATH}
+    fi
 
   # Install the egg and do a sanity check
   $PIP_EXECUTABLE install --force-reinstall --ignore-installed ${EGG_PATH}
